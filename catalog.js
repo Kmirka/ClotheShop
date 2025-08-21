bcc81c0ee67fd8d16ac3868be9019d54f8dabc9c
projectohost <serhiyduo@gmail.com>
2025-08-21 17:42:52 +0300

    Add files via upload

--- catalog.js
+++ catalog.js
@@ -1,6 +1,49 @@
-function addToCart() {
-  alert("Товар додано в кошик!");
+let cart = [];
+
+function addToCart(button) {
+  const product = button.closest(".product");
+  const name = product.querySelector("h3").innerText;
+  const price = parseFloat(product.dataset.price);
+
+  // чи вже є в корзині?
+  const existing = cart.find((item) => item.name === name);
+
+  if (existing) {
+    existing.quantity += 1;
+  } else {
+    cart.push({ name, price, quantity: 1 });
+  }
+console.log(cart);
+
+  renderCart();
 }
+
+function renderCart() {
+  const cartContainer = document.querySelector(".cart-items");
+  const totalEl = document.querySelector(".cart .total");
+
+  cartContainer.innerHTML = "";
+
+  let total = 0;
+
+  cart.forEach((item) => {
+    total += item.price * item.quantity;
+
+    const div = document.createElement("div");
+    div.classList.add("cart-item");
+
+    div.innerHTML = `
+        <span class="item-name">${item.name}</span>
+        <span class="item-qty">x${item.quantity}</span>
+        <span class="item-price">${item.price * item.quantity} грн</span>
+      `;
+
+    cartContainer.appendChild(div);
+  });
+
+  totalEl.textContent = `Всього: ${total} грн`;
+}
+
 
 
 function filterByPrice(range) {
